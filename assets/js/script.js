a1da8f34a841b6a832a17c95383c058479a21b85
Zach Yarbrough <zachyarbro@gmail.com>
2021-07-21 09:13:36 -0500

    add date functionality and calender

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var tasks = {};
+var twoDaysFromNow = moment().add(2, "days");
 
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
@@ -13,6 +14,7 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -121,10 +123,17 @@ $(".list-group").on("click", "span", function() {
     .val(date);
   $(this).replaceWith(dateInput);
 
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      $(this).trigger("change");
+    }
+  });
+
   dateInput.trigger("focus");
 });
 
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   var date = $(this)
     .val()
     .trim();
@@ -142,6 +151,8 @@ $(".list-group").on("blur", "input[type='text']", function() {
     .text(date);
 
   $(this).replaceWith(taskSpan);
+
+  auditTask($(taskSpan).closest(".list-group-item"));
 })
 
 // remove all tasks
@@ -212,6 +223,23 @@ $("#trash").droppable({
     console.log("out");
   }
 })
+
+$('#modalDueDate').datepicker({
+  minDate: 1
+});
+
+var auditTask = function(taskEl) {
+  var date = $(taskEl).find("span")
+    .text()
+    .trim();
+  var time = moment(date, "L").set("hour", 17);
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+  if(moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  } else if(Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+}
 
 // load tasks for the first time
 loadTasks();
